c05756c65615a2b02aa0473f1e995f642232dbf6
Dayton Turner <dayton@voxter.com>
2026-01-26 12:53:23 -0800

    Updated contact blurb

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,9 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        My inbox is always open. Whether you have a question or just want to say hi, feel free to
+        reach out! (Unless you're trying to sell me on the latest SaaS lead generation tooling, in
+        which case, I politely decline.)
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
